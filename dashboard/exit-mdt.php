68ed5955c47e03bdf2c3c0c612fc97a7733dc2e3
BlueHaven1 <havenuebl@gmail.com>
2025-05-13 02:51:23 +0000

    exitmdt

--- dashboard/exit-mdt.php
+++ dashboard/exit-mdt.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+require_once '../includes/supabase.php';
+
+if (isset($_SESSION['user_id'])) {
+    $userId = $_SESSION['user_id'];
+
+    // Remove unit_status for this user
+    supabaseRequest("unit_status?user_id=eq.$userId", "DELETE");
+}
 
 // Unset MDT-related session variables
 unset(
