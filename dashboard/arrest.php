71742d9813171f735d88c5e241b0638c2459ca44
BlueHaven1 <havenuebl@gmail.com>
2025-05-14 13:30:32 +0000

    Update 1.0.0

--- dashboard/arrest.php
+++ dashboard/arrest.php
@@ -87,8 +87,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <a href="arrest.php" class="block px-3 py-2 rounded bg-gray-700">Arrest Report</a>
       <a href="file-warrant.php" class="block px-3 py-2 rounded hover:bg-gray-700">File Warrant</a>
       <a href="serve-warrant.php" class="block px-3 py-2 rounded hover:bg-gray-700">Serve Warrant</a>
-      <a href="penal-code.php" class="block px-3 py-2 rounded hover:bg-gray-700">Penal Code</a>
-      <a href="10-codes.php" class="block px-3 py-2 rounded hover:bg-gray-700">10-Codes</a>
     </nav>
   </div>
   <a href="exit-mdt.php" class="block px-3 py-2 mt-6 rounded bg-red-600 hover:bg-red-700 text-center font-semibold">
--- dashboard/serve-warrant.php
+++ dashboard/serve-warrant.php
@@ -95,8 +95,6 @@ foreach ($penal_titles as $title) {
       <a href="arrest.php" class="block px-3 py-2 rounded hover:bg-gray-700">Arrest Report</a>
       <a href="file-warrant.php" class="block px-3 py-2 rounded hover:bg-gray-700">File Warrant</a>
       <a href="serve-warrant.php" class="block px-3 py-2 rounded bg-gray-700">Serve Warrant</a>
-      <a href="penal-code.php" class="block px-3 py-2 rounded hover:bg-gray-700">Penal Code</a>
-      <a href="10-codes.php" class="block px-3 py-2 rounded hover:bg-gray-700">10-Codes</a>
     </nav>
   </div>
   <a href="exit-mdt.php" class="block px-3 py-2 mt-6 rounded bg-red-600 hover:bg-red-700 text-center font-semibold">
